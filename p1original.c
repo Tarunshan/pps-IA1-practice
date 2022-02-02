1ab1a97709578d4d93039f642bcdbd2dd98c0d37
Tarunshan <94883107+Tarunshan@users.noreply.github.com>
2022-02-02 08:52:36 +0000

    -

--- p1original.c
+++ p1original.c
@@ -1,2 +1,25 @@
+#include <stdio.h>
+void input(int *a,int *b)
+{
+  printf("Enter two numbers ");
+  scanf("%d%d",a,b);
+}
+ 
+ void add(int a,int b,int *sum)
+ {
+   *sum=a+b;
+ }
 
+void output(int a,int b,int sum)
+{
+  printf("The sum of %d and %d is %d",a,b,sum);
+}
+
+int main()
+{
+  int a,b,sum;
+  input(&a,&b);
+  add(a,b,&sum);
+  output(a,b,sum);
+  return 0;
 }
